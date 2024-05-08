3a937f8e90d3d85388359ea4407528002b3c99bc
Jeongdonghun <arrrr180@konkuk.ac.kr>
2024-05-08 15:00:35 +0900

    Initial commit

---
+++ src/main/java/com/example/dormitorybe/DormitoryBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.dormitorybe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DormitoryBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DormitoryBeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dormitorybe/DormitoryBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dormitorybe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DormitoryBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
