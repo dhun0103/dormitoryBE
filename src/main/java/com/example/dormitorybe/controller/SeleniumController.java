5a19b2caf714a83bbc6837c61669e87b1c661249
sohyeonjung <jhyeon23@konkuk.ac.kr>
2024-06-02 01:43:12 +0900

    web driver to chrome driver

--- src/main/java/com/example/dormitorybe/controller/SeleniumController.java
+++ src/main/java/com/example/dormitorybe/controller/SeleniumController.java
@@ -39,13 +39,12 @@ public class SeleniumController {
 
         String result = "";
 
-        List<WebElement> contents;
 
         List<NoticePostReqDto> noticePostReqDtos = new ArrayList<>();
 
         System.out.println("###START###");
 
-        try{
+        try {
             //TODO(path local로 안하는 거 따로 확인)
             Path path = Paths.get("H:\\spring\\dormitoryBE\\chromedriver.exe");
             System.setProperty("webdriver.chrome.driver", path.toString());
@@ -56,26 +55,48 @@ public class SeleniumController {
             options.addArguments("--disable-gpu");  // gpu 비활성화
             options.addArguments("--blink-settings=imagesEnabled=false");   // 이미지 다운 안받음
 
-            WebDriver driver = new ChromeDriver(options);
-            WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 드라이버가 실행된 후 10초 기다림
+//            WebDriver driver = new ChromeDriver(options);
+//            WebDriverWait webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(10)); // 드라이버가 실행된 후 10초 기다림
+
+            ChromeDriver driver = new ChromeDriver(options);
+
+            //빈탭생성
+            driver.executeScript("window.open('about:blank','_blank');");
+
+            //탭목록 가져오기
+            List<String> tabs = new ArrayList<String>(driver.getWindowHandles());
+
+            //첫번쨰 탭으로 전환
+            driver.switchTo().window(tabs.get(0));
 
             driver.get(URL);
 
-//             가져올 데이터가 로딩될 때까지 기다림
-//        webDriverWait.until(
-//                ExpectedCondition.presenceOfElementLocated(By.cssSelector("div._fnctWrap > form:nth-child(2) > div > table > tbody > tr")
-//        );
+//            String totPage = driver.findElement(By.cssSelector("div._fnctWrap > form:nth-child(3) > div > div > p > span._totPage")).getText();
+//            int totalPages = Integer.parseInt(totPage);
+//            WebElement pageDataAttr = driver.findElement(By.cssSelector("#menu2238_obj1168 > div._fnctWrap > form:nth-child(3) > div > div > p > span._totPage"));
+//            System.out.println("######{"+pageDataAttr.getText()+"}#######");
+//           //int totalPages = Integer.parseInt(pageDataAttr);
+            //int totalPages = 0;
+//            String totalPagesText = driver.findElement(By.cssSelector("#menu2238_obj1168 > div._fnctWrap > form:nth-child(3) > div > div > p > span._totPage")).getText();
+//            int totalPages = Integer.parseInt(totalPagesText);
+//            System.out.println("?"+totalPages);
 
-            contents = driver.findElements(By.cssSelector("div._fnctWrap > form:nth-child(2) > div > table > tbody > tr"));
+            //for(int i=0; i<totalPages; i++){
 
-            if(contents.size() > 0) { //데이터 있으면
+//            String pageUrl = URL + "?currentPageNo=" + i;
+//            driver.get(pageUrl);
+
+            List<WebElement> contents = driver.findElements(By.cssSelector("div._fnctWrap > form:nth-child(2) > div > table > tbody > tr"));
+
+            if (contents.size() > 0) { //데이터 있으면
                 for (WebElement content : contents) {
                     //result에 저장
                     String title = content.findElement(By.cssSelector("td.td-subject > a > strong")).getText();
-                    String date = content.findElement(By.cssSelector("td.td-date")).getText();
-                    WebElement url = content.findElement(By.cssSelector("td.td-subject > a"));
 
-                    NoticePostReqDto noticePostReqDto = new NoticePostReqDto(title, date, url.getAttribute("href"));
+                    String date = content.findElement(By.cssSelector("td.td-date")).getText();
+                    String url = content.findElement(By.cssSelector("td.td-subject > a")).getAttribute("href");
+
+                    NoticePostReqDto noticePostReqDto = new NoticePostReqDto(title, date, url);
                     noticePostReqDtos.add(noticePostReqDto);
 
                     //result += "공지 title / 작성일 : " + title + "/" + date + "<br />";
@@ -83,6 +104,11 @@ public class SeleniumController {
 
                 }
             }
+
+
+        //}
+
+
 
             driver.quit();
             System.out.println("####END####");
@@ -92,6 +118,7 @@ public class SeleniumController {
 
         }catch(Exception e){
             System.out.println("####crawling error####");
+            System.out.println(e);
             return GlobalResDto.fail("Failed to create noticePost");
         }
 
