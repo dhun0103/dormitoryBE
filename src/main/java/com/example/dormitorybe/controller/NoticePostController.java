a07d8ce9f3d4a3f15a4134f5714f1f6945e3c70c
Jeongdonghun <arrrr180@konkuk.ac.kr>
2024-06-05 16:48:53 +0900

    fix : refactoring

--- src/main/java/com/example/dormitorybe/controller/NoticePostController.java
+++ src/main/java/com/example/dormitorybe/controller/NoticePostController.java
@@ -1,23 +1,10 @@
 package com.example.dormitorybe.controller;
 
-import com.example.dormitorybe.dto.ReqDto.NoticePostReqDto;
 import com.example.dormitorybe.dto.ResDto.GlobalResDto;
 import com.example.dormitorybe.service.NoticePostService;
-import lombok.RequiredArgsConstructor;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import lombok.RequiredArgsConstructor;;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.io.UnsupportedEncodingException;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
@@ -26,7 +13,7 @@ public class NoticePostController {
 
     private final NoticePostService noticePostService;
 
-    @GetMapping("/notices/kulhouse_notice")
+    @GetMapping("/notice/kulhouse_notice")
     public GlobalResDto getKulhouse_notice(){
         return noticePostService.crawlNoticePost("https://kulhouse.konkuk.ac.kr/home/sub04/sub04_01.asp" +
                 "?intNowPage=1&board_nm=kulhouse_notice&search_m=&search_t=&intNoticeCnt=27");
