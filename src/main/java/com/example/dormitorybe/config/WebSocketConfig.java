cef6c556c656adae43a8d31429a0f12c929f440a
Jeongdonghun <arrrr180@konkuk.ac.kr>
2024-06-09 14:58:09 +0900

    fix : chat

--- src/main/java/com/example/dormitorybe/config/WebSocketConfig.java
+++ src/main/java/com/example/dormitorybe/config/WebSocketConfig.java
@@ -1,23 +1,5 @@
 package com.example.dormitorybe.config;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-
-@RequiredArgsConstructor
-@Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
-    private final WebSocketChatHandler webSocketChatHandler;
-
-    @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(webSocketChatHandler, "ws/chat").setAllowedOrigins("*");
-    }
-}
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
